e45d8f22f0bd5895594bb974e83dde9d16379331
brunos01 <brunosperoni01@gmail.com>
2022-09-11 21:55:17 -0300

    tp9term

---
+++ script.js
@@ -0,0 +1,12 @@
+alert('Hello World!')
+
+var comidas = "Asado"
+var postres = "Dulce de Membrillo"
+var vinos = "Malbec"
+var comidas_postres_vinos = comidas + " " + postres + " " + vinos
+
+comidas = "Pasta"
+postres = "Frutillas"
+
+console.log(comidas, postres, vinos)
+alert(comidas_postres_vinos)
